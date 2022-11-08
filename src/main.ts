5d015f6c6a5dae17237459281a7155410cf99a4e
oguni <>
2022-11-08 21:52:51 +0900

    Add a rain shader

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js"
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js"
 import { createNoise2D } from "simplex-noise"
 // @ts-ignore
-import snoise2DURL from "../node_modules/webgl-noise/src/noise2D.glsl?url"
-const snoise2D = await fetch(snoise2DURL).then((r) => r.text())
+import snoise3DURL from "../node_modules/webgl-noise/src/noise3D.glsl?url"
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+const snoise = (await fetch(snoise3DURL).then((r) => r.text())) + `
+float snoise(vec2 v) {
+    return snoise(vec3(v, 0.0));
+}
+`
 
 const loadGLTF = async (filepath: string, height: number | null): Promise<THREE.Object3D> => {
     const obj = (await new Promise<GLTF>((resolve, reject) => new GLTFLoader().load(filepath, resolve, () => { }, reject)))
@@ -17,9 +22,6 @@ const loadGLTF = async (filepath: string, height: number | null): Promise<THREE.
     }
     return obj
 }
-
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
-camera.position.z = 1
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AmbientLight(0xffffff, 0.025))
@@ -34,13 +36,13 @@ const skybox = await loadGLTF("models/sky_pano_-_grand_canyon_yuma_point_lowres.
 skybox.position.setY(-0.5)
 scene.add(skybox)
 
-const uniforms = {
+const cloudUniforms = {
     time: { value: 0.0 },
 }
 
 const ground = new THREE.Mesh(new THREE.PlaneGeometry(), new THREE.ShaderMaterial({
     transparent: true,
-    uniforms,
+    uniforms: cloudUniforms,
     vertexShader: `\
 out vec2 pos;
 void main() {
@@ -49,7 +51,7 @@ void main() {
 }
 `,
     fragmentShader: `\
-${snoise2D}
+${snoise}
 
 in vec2 pos;
 uniform float time;
@@ -67,6 +69,9 @@ scene.add(ground)
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.outputEncoding = THREE.sRGBEncoding
 
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
+camera.position.set(-0.97, 0.7, 0.25)
+
 // Fit canvas to the window
 renderer.setSize(window.innerWidth, window.innerHeight)
 window.addEventListener("resize", () => {
@@ -79,10 +84,96 @@ const effectComposer = new EffectComposer(renderer)
 effectComposer.addPass(new RenderPass(scene, camera))
 effectComposer.addPass(new UnrealBloomPass(new THREE.Vector2(256, 256), 0.2, 0, 0))
 
+const rainPass = new ShaderPass({
+    uniforms: {
+        tDiffuse: { value: null },
+        aspect: { value: window.innerWidth / window.innerHeight },
+        time: { value: 0.0 },
+    },
+    vertexShader: `
+out vec2 vUv;
+void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}`,
+    fragmentShader: `
+uniform sampler2D tDiffuse;
+uniform float aspect;
+uniform float time;
+in vec2 vUv;
+
+${snoise}
+
+#define scale 1.4
+
+// https://www.desmos.com/calculator/ejrfpu9pyi
+float dropletY(float x, float a) {
+    return mod(x, a) < 1.0 ? sqrt(mod(x, a)) + floor(x/a) : floor(x/a) + 1.0;
+}
+
+#define PI 3.1415926535897932384626433832795
+
+#define horizontalDropletDistributionCycle (2.0 * PI / 300.0)
+
+float horizontalDropletDistribution(float x) {
+    return max(0.0, (cos(x * 300.0) - 0.5) * 2.0);
+}
+
+float movingDropletVolume(vec2 p, float i) {
+    float prob = horizontalDropletDistribution(p.x);
+    if (prob == 0.0) { return 0.0; }
+    p = vec2(p.x + i * horizontalDropletDistributionCycle / 4.0, p.y);
+    float y = dropletY(time * 0.001 + 3.9 * floor(vUv.x / horizontalDropletDistributionCycle - 0.5) + p.y * 2.0, /* cycle of dripping */15.0 + i) * 0.25;
+    float noise = snoise(vec3((p + vec2(0.0, y)) * vec2(aspect, 1.0) * 10.0 / scale, i * 1000.0));
+    return smoothstep(0.01, 1.0, prob * pow(noise, /* higher = sparser droplets */ 10.0));
+}
+
+float fixedDropletVolume(vec2 p) {
+    float noise = snoise(p * vec2(aspect, 1.0) * 60.0 / scale) + snoise((p * vec2(aspect, 1.0) + vec2(100.0, 0.0)) * 60.0 / scale);
+    return smoothstep(0.01, 1.0, pow(noise, /* higher = sparser droplets */ 6.0));
+}
+
+float dropletVolume(vec2 p) {
+    float vol = 0.0;
+    for (float i = 0.0; i < 4.0; i += 1.0) {
+        vol += movingDropletVolume(p + snoise(p) * 0.05, i);
+    }
+    return vol + fixedDropletVolume(p)
+        // splash
+        + smoothstep(0.5, 0.6, pow(snoise(vec3(p * vec2(aspect, 1.0) * 8.0, time * 0.0005)), 8.0)) * 0.05
+        + smoothstep(0.5, 0.6, pow(snoise(vec3(p * vec2(aspect, 1.0) * 8.0, time * 0.0005 + 100.0)), 8.0)) * 0.05
+        + smoothstep(0.5, 0.6, pow(snoise(vec3(p * vec2(aspect, 1.0) * 8.0, time * 0.0005 + 200.0)), 8.0)) * 0.05;
+}
+
+vec2 dropletGradient(vec2 p) {
+    float base = dropletVolume(p);
+    if (base == 0.0) { return vec2(0.0); }
+    // return vec2(
+    //     dropletVolume(p + vec2(0.0005 * scale, 0.0)) / base - 1.0,
+    //     dropletVolume(p + vec2(0.0, 0.0005 * scale)) / base - 1.0
+    // );
+    return vec2(
+        base - dropletVolume(p + vec2(0.0005 * scale, 0.0)),
+        base - dropletVolume(p + vec2(0.0, 0.0005 * scale))
+    );
+}
+
+void main() {
+    if (dropletVolume(vUv) > 0.001) {
+        gl_FragColor = pow(texture2D(tDiffuse, vUv + dropletGradient(vUv) * /* lower = transparent */9.0), vec4(vec3(/* lower = brighter */ 0.9), 1.0));
+    } else {
+        gl_FragColor = texture2D(tDiffuse, vUv);
+    }
+}`
+})
+effectComposer.addPass(rainPass)
+
 // Main loop
 const rotationNoise = createNoise2D()
 renderer.setAnimationLoop((time: number): void => {
-    uniforms.time.value = time
+    cloudUniforms.time.value = time
+    rainPass.uniforms.aspect.value = camera.aspect
+    rainPass.uniforms.time.value = time
     balloon.rotation.x = rotationNoise(0, time * 0.0003) * (1.5 / 180 * Math.PI)
     balloon.rotation.y = rotationNoise(1, time * 0.0003) * (1.5 / 180 * Math.PI)
     balloon.rotation.z = rotationNoise(2, time * 0.0003) * (1.5 / 180 * Math.PI)
