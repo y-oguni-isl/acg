8e83c6c37e0b9ff07d872fc464907122acda8948
oguni <>
2022-11-10 19:55:04 +0900

    Add checkboxes for debugging

--- src/main.ts
+++ src/main.ts
@@ -28,16 +28,31 @@ const loadGLTF = async (filepath: string, height: number | null): Promise<THREE.
     return obj
 }
 
+const option = (name: string, defaultValue: boolean = true) => {
+    const label = document.querySelector<HTMLTemplateElement>("#optionTemplate")!.content.children[0].cloneNode(true) as HTMLLabelElement
+    const input = label.querySelector("input")!
+    input.checked = defaultValue ?
+        localStorage.getItem(`option.${name}`) !== "false" :
+        localStorage.getItem(`option.${name}`) === "true"
+    input.addEventListener("click", () => {
+        localStorage.setItem(`option.${name}`, input.checked + "")
+        location.reload()
+    })
+    label.querySelector("span")!.innerText = name
+    document.querySelector<HTMLDivElement>("#options")!.append(label)
+    return input.checked
+}
+
 const scene = new THREE.Scene()
 scene.add(new THREE.AmbientLight(0xffffff, 0.025))
 const directionalLight = new THREE.DirectionalLight(0xf5eeba, 1.6)
 directionalLight.position.set(0.3, 1, -1)
 scene.add(directionalLight)
 
-const airplane = await loadGLTF("models/low-poly_airplane.glb", 0.2)
+const airplane = !option("airplane") ? new THREE.Object3D() : await loadGLTF("models/low-poly_airplane.glb", 0.05)
 scene.add(airplane)
 
-const skybox = await loadGLTF("models/sky_pano_-_grand_canyon_yuma_point_lowres.glb", 4)
+const skybox = !option("skybox") ? new THREE.Object3D() : await loadGLTF("models/sky_pano_-_grand_canyon_yuma_point_lowres.glb", 4)
 skybox.rotateX(-Math.PI / 2)
 skybox.position.setY(-0.5)
 scene.add(skybox)
@@ -45,13 +60,13 @@ scene.add(skybox)
 document.querySelector<HTMLDivElement>("#message")!.innerText = `Loading models...`
 await new Promise((resolve) => setTimeout(resolve, 0)) // Render DOM
 
-const cloudUniforms = {
+const timeUniform = {
     time: { value: 0.0 },
 }
 
-const ground = new THREE.Mesh(new THREE.PlaneGeometry(), new THREE.ShaderMaterial({
+const fog = !option("fog") ? new THREE.Object3D() : new THREE.Mesh(new THREE.PlaneGeometry(), new THREE.ShaderMaterial({
     transparent: true,
-    uniforms: cloudUniforms,
+    uniforms: timeUniform,
     vertexShader: /* glsl */`\
 out vec2 pos;
 void main() {
@@ -69,17 +84,47 @@ void main() {
 }
 `
 }))
+fog.name = "fog"
+fog.rotateX(-Math.PI / 2)
+fog.scale.setScalar(4)
+fog.position.setY(-0.13)
+scene.add(fog)
 
-ground.rotateX(-Math.PI / 2)
-ground.scale.setScalar(4)
-ground.position.setY(-0.13)
-scene.add(ground)
+const laser = !option("laser", false) ? new THREE.Object3D() : new THREE.Mesh(new THREE.PlaneGeometry(1, 1), new THREE.ShaderMaterial({
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    uniforms: timeUniform,
+    vertexShader: /* glsl */`\
+out vec2 pos;
+void main() {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    pos = position.xy;
+}
+`,
+    fragmentShader: /* glsl */`\
+in vec2 pos;
+uniform float time;
+
+void main() {
+    float opacityY = (sin(pos.y * 30.0 - time * 0.08) + 1.0) / 2.0;
+    float opacityX = max(0.0, 1.0 - abs(pos.x + cos(time * 0.08) * 0.02) * 100.0);
+    gl_FragColor = vec4(0.0, 1.0, 1.0, opacityY * opacityX);
+}
+`
+}))
+laser.name = "laser"
+laser.renderOrder = 1
+laser.rotateY(-Math.PI / 2)
+laser.rotateX(-Math.PI / 2)
+laser.scale.set(0.25, 2, 0)
+laser.position.set(1, 0.01, 0)
+scene.add(laser)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.outputEncoding = THREE.sRGBEncoding
 
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
-camera.position.set(-1, 0.7, 0)
+camera.position.set(-0.5, 0.6, 0)
 
 // Fit canvas to the window
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -91,24 +136,37 @@ window.addEventListener("resize", () => {
 
 const effectComposer = new EffectComposer(renderer)
 effectComposer.addPass(new RenderPass(scene, camera))
-effectComposer.addPass(new UnrealBloomPass(new THREE.Vector2(256, 256), 0.2, 0, 0))
+if (option("unrealbloom")) { effectComposer.addPass(new UnrealBloomPass(new THREE.Vector2(256, 256), 0.2, 0, 0)) }
 
 const numMouseTrails = 10
 const declareMouseUniform = Array.from(Array(numMouseTrails).keys(), (i) => `uniform vec2 mouse${i};`).join("\n")
-const rainPass = new ShaderPass({
-    uniforms: {
-        tDiffuse: { value: null },
-        aspect: { value: window.innerWidth / window.innerHeight },
-        time: { value: 0.0 },
-        ...Object.fromEntries(Array.from(Array(numMouseTrails).keys(), (i) => [`mouse${i}`, { value: new THREE.Vector2(0.5, 0.5) }])),
-    },
-    vertexShader: /* glsl */`
+let rainPass: ShaderPass | null = null
+if (option("rain")) {
+    const rainBlur = !option("rain.blur", false) ? "" : /* glsl */`gl_FragColor = vec4(
+        texture2D(tDiffuse, vUv + vec2(-0.0028,  0.0028) / vec2(aspect, 1.0)) +
+        texture2D(tDiffuse, vUv + vec2(-0.0040,  0.0000) / vec2(aspect, 1.0)) +
+        texture2D(tDiffuse, vUv + vec2(-0.0028, -0.0028) / vec2(aspect, 1.0)) +
+        texture2D(tDiffuse, vUv + vec2( 0.0000,  0.0040) / vec2(aspect, 1.0)) +
+        texture2D(tDiffuse, vUv) +
+        texture2D(tDiffuse, vUv + vec2( 0.0000, -0.0040) / vec2(aspect, 1.0)) +
+        texture2D(tDiffuse, vUv + vec2( 0.0028,  0.0028) / vec2(aspect, 1.0)) +
+        texture2D(tDiffuse, vUv + vec2( 0.0040,  0.0000) / vec2(aspect, 1.0)) +
+        texture2D(tDiffuse, vUv + vec2( 0.0028, -0.0028) / vec2(aspect, 1.0))
+    ) / 9.0;`
+    rainPass = new ShaderPass({
+        uniforms: {
+            tDiffuse: { value: null },
+            aspect: { value: window.innerWidth / window.innerHeight },
+            time: { value: 0.0 },
+            ...Object.fromEntries(Array.from(Array(numMouseTrails).keys(), (i) => [`mouse${i}`, { value: new THREE.Vector2(0.5, 0.5) }])),
+        },
+        vertexShader: /* glsl */`
 out vec2 vUv;
 void main() {
     vUv = uv;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }`,
-    fragmentShader: /* glsl */`
+        fragmentShader: /* glsl */`
 uniform sampler2D tDiffuse;
 uniform float aspect;
 uniform float time;
@@ -182,17 +240,7 @@ void main() {
         gl_FragColor = texture2D(tDiffuse, vUv);
 
         // blur
-        // gl_FragColor = vec4(
-        //     texture2D(tDiffuse, vUv + vec2(-0.0028,  0.0028) / vec2(aspect, 1.0)) +
-        //     texture2D(tDiffuse, vUv + vec2(-0.0040,  0.0000) / vec2(aspect, 1.0)) +
-        //     texture2D(tDiffuse, vUv + vec2(-0.0028, -0.0028) / vec2(aspect, 1.0)) +
-        //     texture2D(tDiffuse, vUv + vec2( 0.0000,  0.0040) / vec2(aspect, 1.0)) +
-        //     texture2D(tDiffuse, vUv) +
-        //     texture2D(tDiffuse, vUv + vec2( 0.0000, -0.0040) / vec2(aspect, 1.0)) +
-        //     texture2D(tDiffuse, vUv + vec2( 0.0028,  0.0028) / vec2(aspect, 1.0)) +
-        //     texture2D(tDiffuse, vUv + vec2( 0.0040,  0.0000) / vec2(aspect, 1.0)) +
-        //     texture2D(tDiffuse, vUv + vec2( 0.0028, -0.0028) / vec2(aspect, 1.0))
-        // ) / 9.0;
+        ${rainBlur}
 
         gl_FragColor = pow(gl_FragColor, vec4(vec3(0.7), 1.0));
 
@@ -213,36 +261,39 @@ void main() {
         gl_FragColor = mix(texture2D(tDiffuse, vUv), gl_FragColor, opacity);
     }
 }`
-})
-effectComposer.addPass(rainPass)
+    });
+    effectComposer.addPass(rainPass)
+    const rotateMouseTrail = () => {
+        for (let i = numMouseTrails - 2; i >= 0; i--) {
+            rainPass!.uniforms[`mouse${i + 1}`].value.copy(rainPass!.uniforms[`mouse${i}`].value)
+        }
+        rainPass!.uniforms.mouse0.value.set(-1.0, -1.0, -1.0)
+    }
+
+    window.addEventListener("mousemove", (ev) => {
+        (rainPass!.uniforms.mouse0.value as THREE.Vector2).set(ev.pageX / window.innerWidth, 1 - ev.pageY / window.innerHeight)
+        if ((rainPass!.uniforms.mouse0.value as THREE.Vector2).distanceTo(rainPass!.uniforms.mouse1.value as THREE.Vector2) > 0.1) {
+            rotateMouseTrail()
+        }
+    })
+
+    setInterval(() => { rotateMouseTrail() }, 100)
+}
 
 // Main loop
 const rotationNoise = createNoise2D()
 renderer.setAnimationLoop((time: number): void => {
-    cloudUniforms.time.value = time
-    rainPass.uniforms.aspect.value = camera.aspect
-    rainPass.uniforms.time.value = time;
+    timeUniform.time.value = time
+    if (rainPass !== null) {
+        rainPass.uniforms.aspect.value = camera.aspect
+        rainPass.uniforms.time.value = time;
+    }
     airplane.rotation.x = rotationNoise(0, time * 0.0003) * (4 / 180 * Math.PI)
     airplane.rotation.y = Math.PI * 0.5 + rotationNoise(1, time * 0.0003) * (4 / 180 * Math.PI)
     airplane.rotation.z = rotationNoise(2, time * 0.0003) * (4 / 180 * Math.PI)
+
     effectComposer.render()
 })
-
-const rotateMouseTrail = () => {
-    for (let i = numMouseTrails - 2; i >= 0; i--) {
-        rainPass.uniforms[`mouse${i + 1}`].value.copy(rainPass.uniforms[`mouse${i}`].value)
-    }
-    rainPass.uniforms.mouse0.value.set(-1.0, -1.0, -1.0)
-}
-
-window.addEventListener("mousemove", (ev) => {
-    (rainPass.uniforms.mouse0.value as THREE.Vector2).set(ev.pageX / window.innerWidth, 1 - ev.pageY / window.innerHeight)
-    if ((rainPass.uniforms.mouse0.value as THREE.Vector2).distanceTo(rainPass.uniforms.mouse1.value as THREE.Vector2) > 0.1) {
-        rotateMouseTrail()
-    }
-})
-
-setInterval(() => { rotateMouseTrail() }, 100)
 
 new OrbitControls(camera, renderer.domElement).listenToKeyEvents(window)
 
