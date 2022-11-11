aaf27e48c272860ed7d82326a1b149d27bbdfd2f
oguni <>
2022-11-11 19:38:08 +0900

    Correct brightness

--- src/shader/selective_bloom.ts
+++ src/shader/selective_bloom.ts
@@ -33,7 +33,9 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 in vec2 vUv;
 void main() {
-    gl_FragColor = texture2D(texture1, vUv) + texture2D(texture2, vUv);
+    gl_FragColor = texture2D(texture1, vUv);
+    vec4 color2 = texture2D(texture2, vUv);
+    gl_FragColor.rgb = pow(gl_FragColor.rgb + color2.rgb * color2.a, /* TODO: I don't know why but using this filter makes the display darker, so I've corrected it here. */vec3(1.0 / 1.9));
 }`,
             defines: {},
         }),
