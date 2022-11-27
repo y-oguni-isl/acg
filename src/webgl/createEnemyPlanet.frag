30be54ac40ccf2063b97a674d07c787ad9f582e4
oguni <>
2022-11-27 11:38:05 +0900

    Use .frag,.vert instead of .frag.ts,.vert.ts

---
+++ src/webgl/createEnemyPlanet.frag
@@ -0,0 +1,6 @@
+uniform float time;
+
+void main() {
+    super();
+    // gl_FragColor.rgb *= 1.0 + (sin(time * 0.006) + 1.0) / 2.0 * 10.0;
+}
--- src/webgl/createEnemyPlanet.ts
+++ src/webgl/createEnemyPlanet.ts
@@ -1,18 +1,11 @@
 import { onBeforeRender } from "../hooks"
 import { enableSelectiveBloom } from "./createSelectiveBloomPass"
 import { extendMaterial, loadGLTF, ObjectPool } from "./webglUtil"
+import fragmentShader from "./createEnemyPlanet.frag"
 
 export default async () => {
     const m = await loadGLTF("models/planet_megatropolis.glb", 2)
-    extendMaterial(m, {
-        fragmentShader: /* glsl */`
-uniform float time;
-
-void main() {
-super();
-// gl_FragColor.rgb *= 1.0 + (sin(time * 0.006) + 1.0) / 2.0 * 10.0;
-}`
-    })
+    extendMaterial(m, { fragmentShader })
     return new ObjectPool("enemyPlanet", enableSelectiveBloom(m))
         .onClone((copy) => {
             onBeforeRender.add((time) => {
