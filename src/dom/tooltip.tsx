24edf389678a9c7e201c2480eccdb9dddb59b8fb
oguni <>
2022-12-10 23:33:56 +0900

    Animate tooltips

--- src/dom/tooltip.tsx
+++ src/dom/tooltip.tsx
@@ -10,15 +10,16 @@ import { createStore } from "../util"
 const store = createStore({
     key: "",
     content: null as ComponentChildren,
+    visible: false,
 }, () => { })
 
 /** Sets the tooltip text for the given key, overwriting any existing tooltip. */
-export const setTooltip = (key: string, content: ComponentChildren) => { store.setState({ key, content }) }
+export const setTooltip = (key: string, content: ComponentChildren) => { store.setState({ key, content, visible: true }) }
 
 /** Removes the tooltip text for the given key if the currently displayed tooltip's key matches the given key. */
 export const removeTooltip = (key: string) => {
     if (store.getState().key === key) {
-        store.setState({ key: "", content: null })
+        store.setState({ key: "", visible: false })
     }
 }
 
@@ -27,6 +28,7 @@ export const Tooltip = () => {
     const [mouseX, setMouseX] = useState(0)
     const [mouseY, setMouseY] = useState(0)
     const content = useStore(store, (s) => s.content)
+    const visible = useStore(store, (s) => s.visible)
     useEffect(() => {
         const onMousemove = (ev: MouseEvent) => {
             setMouseX(ev.clientX)
@@ -34,10 +36,12 @@ export const Tooltip = () => {
         }
         window.addEventListener("mousemove", onMousemove)
     }, [])
-    if (!content) { return <></> }
-    return <div style={mouseX < window.innerWidth / 2 ?
-        { left: `${mouseX + 50}px`, top: `${mouseY}px` } :
-        { right: `${window.innerWidth - mouseX + 50}px`, top: `${mouseY}px` }} class={"text-gray-100 absolute px-6 py-1 max-w-[300px] backdrop-blur-3xl bg-[linear-gradient(240deg,rgba(31,37,46,0.4)_0%,rgba(30,36,44,0.4)_100%)] pointer-events-none rounded-sm [font-size:80%] [-webkit-text-stroke:5px_rgba(255,255,255,0.15)]"}>
+    return <div style={{
+        transform: visible ? "translateY(0%) scaleY(100%)" : "translateY(-50%) scaleY(0%)",
+        ...(mouseX < window.innerWidth / 2 ?
+            { left: `${mouseX + 50}px`, top: `${mouseY}px` } :
+            { right: `${window.innerWidth - mouseX + 50}px`, top: `${mouseY}px` }),
+    }} class={"text-gray-100 absolute px-6 py-1 max-w-[300px] backdrop-blur-3xl bg-[linear-gradient(240deg,rgba(31,37,46,0.4)_0%,rgba(30,36,44,0.4)_100%)] pointer-events-none rounded-sm [font-size:80%] [-webkit-text-stroke:5px_rgba(255,255,255,0.15)] [transition:transform_0.1s_ease-out]"}>
         {content}
     </div>
 }
