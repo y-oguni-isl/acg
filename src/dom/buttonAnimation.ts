f5d17bae2f7d479e201dde86892fd4e402519d1c
oguni <>
2022-12-18 12:31:41 +0900

    Don't play animation when clicking disabled buttons

--- src/dom/buttonAnimation.ts
+++ src/dom/buttonAnimation.ts
@@ -1,7 +1,7 @@
 // Add animations to buttons clicked
 window.addEventListener("click", (ev) => {
     let button: HTMLElement | null
-    if (ev.target instanceof Element && (button = ev.target.closest("button,.button") as HTMLElement | null)) {
+    if (ev.target instanceof Element && (button = ev.target.closest("button:not(:disabled),.button.pointer") as HTMLElement | null)) {
         button.style.animation = ""
         requestAnimationFrame(() => { requestAnimationFrame(() => { button!.style.animation = "buttonClickAnimation 0.2s linear forwards" }) })
     }
