fe4b90078dead1b45ba42600f51f7e4fe96f5f44
oguni <>
2022-12-18 12:31:48 +0900

    Simplify the implementation of stageTransitionPass

--- src/webgl/createStageTransitionPass.frag
+++ src/webgl/createStageTransitionPass.frag
@@ -1,13 +1,7 @@
 uniform sampler2D tDiffuse;
 in vec2 vUv;
-uniform float time;  // in [0, 2]
+uniform float opacity;  // in [0, 1]
 
 void main() {
-    if (time < 1.0) {
-        // fade in
-        gl_FragColor = mix(texture2D(tDiffuse, vUv), vec4(0.0, 0.0, 0.0, 1.0), time);
-    } else {
-        // fade out
-        gl_FragColor = mix(texture2D(tDiffuse, vUv), vec4(0.0, 0.0, 0.0, 1.0), 2.0 - time);
-    }
+    gl_FragColor = mix(texture2D(tDiffuse, vUv), vec4(0.0, 0.0, 0.0, 1.0), opacity);
 }
--- src/webgl/createStageTransitionPass.ts
+++ src/webgl/createStageTransitionPass.ts
@@ -7,36 +7,37 @@ import vertexShader from "./createStageTransitionPass.vert"
 
 /** Creates a ShaderPass (postprocessing shader) that is applied when the player is moving to other stage. */
 export default () => {
-    let played = false
     const pass = new ShaderPass({
         uniforms: {
             tDiffuse: { value: null },
-            time: { value: 0 },
+            opacity: { value: 0 },
         },
         vertexShader,
         fragmentShader,
     })
+    pass.enabled = false
     return {
         pass,
         /** This function fades in for 1 second, moves to the destination stage and calls the callback, and fades out for 1 second.  */
         play: (callback: () => void) => {
-            if (played) { return }
-            played = true
+            if (pass.enabled) { return }
+            pass.enabled = true
             let time = 0
             const loop = () => {
                 if (time < 1) { // fade in
                     time = Math.min(1, time + 1 / updatePerSecond)
+                    pass.uniforms.opacity!.value = time
                 } else if (time === 1) { // move to the destination stage
                     time += 1 / updatePerSecond
                     callback()
                     getState().completeStageTransition()
                 } else if (time < 2) { // fade out
                     time = Math.min(2, time + 1 / updatePerSecond)
+                    pass.uniforms.opacity!.value = 2 - time
                 } else { // end
                     onUpdate.delete(loop)
-                    played = false
+                    pass.enabled = false
                 }
-                pass.uniforms.time!.value = time
             }
             onUpdate.add(loop)
         }
